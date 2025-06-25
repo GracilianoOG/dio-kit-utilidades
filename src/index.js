f2f1a586eb22ba39fcdad915031eba9d4c619431
Gabriel <gabriel.gracilianomb@gmail.com>
2025-06-24 21:40:55 -0300

    Call createQRCode if choice is 1

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import prompt from "prompt";
 import { mainPrompt } from "./prompts/mainPrompt.js";
+import createQRCode from "./services/qr-code/create.js";
 
 const main = () => {
   prompt.get(mainPrompt, (err, choice) => {
     switch (parseInt(choice.select)) {
       case 1:
-        console.log("QRCODE");
+        createQRCode();
         break;
       case 2:
         console.log("PASSWORD GENERATOR");
