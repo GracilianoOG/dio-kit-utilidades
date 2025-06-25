efca3de4f0bce1b3947625066dc434b58e51687a
Gabriel <gabriel.gracilianomb@gmail.com>
2025-06-24 22:09:41 -0300

    Show password created

--- src/services/password/create.js
+++ src/services/password/create.js
@@ -1,7 +1,10 @@
 import chalk from "chalk";
+import handle from "./handle.js";
 
 const createPassword = () => {
-  console.log(chalk.green("password"));
+  console.log(chalk.green.italic("\nPassword generated!\n"));
+  const password = handle();
+  console.log(chalk.white.bgMagenta(password));
 };
 
 export default createPassword;
