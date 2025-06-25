0e86ce7b12bbf4df765b1c210924d9d5cf7fb2d7
Gabriel <gabriel.gracilianomb@gmail.com>
2025-06-24 23:03:48 -0300

    Rename to qrcodeSchema

---
+++ src/prompts/schemas/qrcodeSchema.js
@@ -0,0 +1,17 @@
+import chalk from "chalk";
+
+export const qrcodeSchema = [
+  {
+    name: "link",
+    description: chalk.blue.bold("Digite o link para gerar o QR Code: "),
+  },
+  {
+    name: "type",
+    description: chalk.blue.bold(
+      "Escolha entre o tipo de QR Code:\n(1) - NORMAL\n(2) - TERMINAL\nOPÇÃO"
+    ),
+    pattern: /^[1-2]+$/,
+    message: chalk.red.italic("Escolha apenas entre 1 e 2"),
+    required: true,
+  },
+];
--- src/services/qr-code/create.js
+++ src/services/qr-code/create.js
@@ -1,9 +1,9 @@
 import prompt from "prompt";
-import { qrcodePrompt } from "../../prompts/qrcodePrompt.js";
+import { qrcodeSchema } from "../../prompts/schemas/qrcodeSchema.js";
 import handle from "./handle.js";
 
 const createQRCode = () => {
-  prompt.get(qrcodePrompt, handle);
+  prompt.get(qrcodeSchema, handle);
 
   prompt.start();
 };
