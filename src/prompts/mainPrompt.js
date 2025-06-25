a9aeb56f86325b067be372a3269b00c00f5e7f6b
Gabriel <gabriel.gracilianomb@gmail.com>
2025-06-24 22:23:26 -0300

    Improve menu prompt

--- src/prompts/mainPrompt.js
+++ src/prompts/mainPrompt.js
@@ -4,7 +4,7 @@ export const mainPrompt = [
   {
     name: "select",
     description: chalk.blue.bold(
-      "Escolha a ferramenta (1 - QRCODE ou (2 - PASSWORD"
+      "Escolha a ferramenta desejada:\n(1) - QRCODE\n(2) - PASSWORD GENERATOR\nOPÇÃO"
     ),
     pattern: /^[1-2]+$/,
     message: chalk.red.italic("Escolha apenas entre 1 e 2"),
